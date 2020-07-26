9f4bfa65360889fdbe2e92dab27a4cf78c98c888
TaufiqRizky <taufiq.it2@gmail.com>
2020-07-26 22:11:22 +0700

    login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -17,12 +17,12 @@
     
 
     <div class="container">
-      <h4 class="text-center text-danger">FORM LOGIN</h4>
+      <h4 class="text-center text-dark">FORM LOGIN</h4>
       <hr>
 
       <form method="post" action="{{ route('login') }}">
         @csrf
-     <div class="form-group  text-danger"> 
+     <div class="form-group  text-dark"> 
         <label >E-Mail</label>
         <div class="input-group">
           <div class="input-group-prepend">
@@ -38,7 +38,7 @@
         </div>
      </div>
 
-    <div class="form-group text-danger"> 
+    <div class="form-group text-dark"> 
       <label>Password</label>
       <div class="input-group">
         <div class="input-group-prepend">
